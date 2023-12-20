39784783096556f91a48ea2c2d78475506085ea3
ScoutLondon <mynewhousenc@gmail.com>
2023-12-20 18:26:54 -0500

    Added SecurityFilterChain to SecurityConfig

--- webapp/src/main/java/com/bogglebros/webapp/config/SecurityConfig.java
+++ webapp/src/main/java/com/bogglebros/webapp/config/SecurityConfig.java
@@ -6,15 +6,55 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //boilerplate to disable csrf and authorize requests
+        http.csrf(csrf -> csrf.disable());
+
+        //determines which requests are authenticated
+        //any part of your application that requires you to know who is logged in to take some action
+        //must be listed here and have its own controller to handle those requests
+        http.authorizeRequests()
+                .requestMatchers(
+                        //this will make all requests to /customer/** require authentication
+                        //we will now have to authenticate to user our customer search or customer create pages
+                        new AntPathRequestMatcher("/order/**"),
+
+                        new AntPathRequestMatcher("/reservation/**"),
+                        new AntPathRequestMatcher("/user/**/")).authenticated()
+                .anyRequest().permitAll();
+
+        //tells us the URL for the login page and the URL to submit the login form
+        http.formLogin(formLogin -> formLogin
+                //this is the URL for the login page
+                .loginPage("/auth/login")
+                //this is the URL to submit the login form
+                .loginProcessingUrl("/auth/loginSubmit"));
+
+        // this is telling spring security to logout when we hit the /login/logout URL
+        http.logout(formLogout -> formLogout
+                .invalidateHttpSession(true)
+                // this is the URL to submit the logout form
+                .logoutUrl("/auth/logout")
+                // this is the URL to go to after logout
+                .logoutSuccessUrl("/"));
+
+        return http.build();
+    }
 
     //boilerplate to add BCrypt password encoder
     @Bean(name = "passwordEncoder")
