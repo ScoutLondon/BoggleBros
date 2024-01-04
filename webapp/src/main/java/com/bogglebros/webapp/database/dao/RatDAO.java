9ea96c04cc95cd88500995dff12b9cb9a8e1bddd
ScoutLondon <mynewhousenc@gmail.com>
2024-01-04 14:26:28 -0500

    Added ratDAO query to create list of rats with given status

--- webapp/src/main/java/com/bogglebros/webapp/database/dao/RatDAO.java
+++ webapp/src/main/java/com/bogglebros/webapp/database/dao/RatDAO.java
@@ -5,11 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface RatDAO extends JpaRepository<Rat, Long> {
     public Rat findById(Integer id);
 
     public Rat findByName(String name);
+
+    @Query("SELECT r from Rat r WHERE r.ratStatus = :ratStatus")
+    List<Rat> findByRatStatus(String ratStatus);
 
 }
